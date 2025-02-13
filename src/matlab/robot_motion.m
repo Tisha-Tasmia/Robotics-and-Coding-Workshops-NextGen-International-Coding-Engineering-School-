5f7813d051d8c88ba7adaceab3c554f737606928
Tasmia Hoque Tisha <tishaa.hoque@gmail.com>
2025-02-14 03:37:20 +0600

    Update robot_motion.m

--- src/matlab/robot_motion.m
+++ src/matlab/robot_motion.m
@@ -1,1 +1,17 @@
 
+clc; clear; close all;
+
+% Define robot parameters
+robotSpeed = 2; % speed in m/s
+timeInterval = 0:0.1:10; % time vector
+
+% Compute distance traveled
+distance = robotSpeed * timeInterval;
+
+% Plot robot movement
+figure;
+plot(timeInterval, distance, 'b-', 'LineWidth', 2);
+xlabel('Time (seconds)');
+ylabel('Distance (meters)');
+title('Robot Motion Control');
+grid on;
